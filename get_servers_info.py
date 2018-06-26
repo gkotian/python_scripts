24d90b65833181d0bad8ce6e726fa28695b160c2
Gautam Kotian <gautam.kotian@gmail.com>
2018-06-26 23:31:23 +0200

    Allow automatic comparison of servers via csv file
    When there are more than 5 servers in a cluster, then it can become
    tedious to compare the list obtained via the cssh config with the list
    in foreman. In such cases, make an automatic comparison using the csv
    file saved from foreman.
    
    Fixes #10.

--- get_servers_info.py
+++ get_servers_info.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import argparse
+import os.path
 import subprocess
 import sys
 
@@ -57,6 +58,53 @@ def getInterestLevelOfLine(line_header, app_being_searched, region_being_searche
                 return 'PARTIAL'
 
     return 'UNINTERESTING'
+
+
+def csv_file_check(cssh_config_server_list):
+    cssh_config_server_list = sorted(cssh_config_server_list)
+
+    csv_file = raw_input('Enter the path of the csv file for comparison (q to quit): ')
+
+    if csv_file == 'q':
+        return
+
+    if not os.path.isfile(csv_file):
+        # Maybe the user entered only the file name instead of the full path,
+        # check if the file exists in the '/tmp' directory
+        csv_file = '/tmp/' + csv_file
+        if not os.path.isfile(csv_file):
+            print ("Couldn't find the csv file, no automatic check possible.")
+            return
+
+    csv_file_server_list = []
+
+    with open(csv_file, 'r') as in_file:
+        line_num = 0
+        for line in in_file:
+            line_num += 1
+
+            # Assume that the first line is the csv header
+            if line_num == 1:
+                continue
+
+            line = line.strip()
+
+            if len(line) == 0:
+                continue
+
+            server = line.split('.')[0]
+
+            csv_file_server_list.append(server)
+
+    csv_file_server_list = sorted(csv_file_server_list)
+
+    if cssh_config_server_list != csv_file_server_list:
+        print("The list of servers obtained via the cssh config doesn't match the list of servers obtained via the csv file")
+        list1 = [obj for obj in cssh_config_server_list if obj not in csv_file_server_list]
+        list2 = [obj for obj in csv_file_server_list if obj not in cssh_config_server_list]
+        print('Mismatching servers: {}'.format(list1 + list2))
+    else:
+        print("It's a match!")
 
 
 parser = argparse.ArgumentParser(usage='%(prog)s app region [-f]',
@@ -137,3 +185,8 @@ if args['launch_foreman']:
     (stdout, stderr) = proc.communicate()
 
     print('Launched a new browser tab to get the latest info from foreman')
+
+    # If there are more than 5 servers, then give the user the option to enter a
+    # csv file for comparison
+    if len(final_list) > 5:
+        csv_file_check(final_list)
